95197b9ab250bd0a8a70fee8acbe7cd94a24878c
unlic <oeh2694@gmail.com>
2023-04-24 14:12:58 +0300

    изменено:      app.js

--- app.js
+++ app.js
@@ -22,6 +22,34 @@ if(listOfLogs!=null)
     var element = document.querySelector('.company-id');
     var companyId = element.querySelector('div').textContent;
 
+    
+
+    var myListStrings = originalText.split("\n");
+    regex = /outeID=(\d+)/; 
+    var regexAux = /(.*Playing 'vOffice\/\d+\/production\/voice\/)(.*?)('.*)/;
+    for (var i = 0; i < myListStrings.length; i++) 
+    {
+        routeId = myListStrings[i].match(regex);
+        audioSrc = myListStrings[i].match(regexAux);
+        var oldString = myListStrings[i];
+        if(myListStrings[i].match(regex))
+        {
+            myListStrings[i] = myListStrings[i].replace(myListStrings[i].match(regex)[1], `<a target="_blank" href="https://panel.binotel.com/?module=routes&companyID=${companyId}&action=edit&routeID=${routeId[1]}&type=callRules">${routeId[1]}</a>`);
+            originalText = originalText.replace(oldString,myListStrings[i]);
+        }
+        if(myListStrings[i].match(regexAux))
+        {
+            var audioSrc = myListStrings[i].match(regexAux);
+            var textBefore = audioSrc[1];
+            var textAfter = audioSrc[3];
+            var audioSrc = `https://panel.binotel.com/public/vOffice/${companyId}/origin/voice/${audioSrc[2].replace('.alaw','.wav').replace('.gsm','.wav')}`;
+
+            myListStrings[i] = oldString + `<audio controls src="${audioSrc}">${audioSrc[2].replace('.alaw','.wav')}</audio><br>`;
+            originalText = originalText.replace(oldString,myListStrings[i]);
+        }
+    
+    }
+    
     var regex = /vOfficeIncomingCall,s,1\((\d+,\d+,\d+)\)/; 
     var text = originalText.match(regex); 
 
@@ -32,34 +60,12 @@ if(listOfLogs!=null)
     var numberIdNode = document.createTextNode(`<a target="_blank" href="https://panel.binotel.com/?module=pbxNumbersEnhanced&companyID=${companyId}&action=edit&pbxNumberID=${numberId}">${numberId}</a>`);
 
     var myString = originalText.replace(originalText.match(regex)[1].split(",")[1], `<a target="_blank" href="https://panel.binotel.com/?module=routes&companyID=${companyId}&action=edit&routeID=${routeId}&type=callRules">${routeId}</a>`);
-    var myString = myString.replace(originalText.match(regex)[1].split(",")[2], `<a target="_blank" href="https://panel.binotel.com/?module=pbxNumbersEnhanced&companyID=${companyId}&action=edit&pbxNumberID=${numberId}">${numberId}</a>`);
+    myString = myString.replace(originalText.match(regex)[1].split(",")[2], `<a target="_blank" href="https://panel.binotel.com/?module=pbxNumbersEnhanced&companyID=${companyId}&action=edit&pbxNumberID=${numberId}">${numberId}</a>`);
 
-
-    var myListStrings = myString.split("\n");
-    regex = /ivrRouteID=(\d+)/; 
-    var regexAux = /(.*Playing 'vOffice\/\d+\/production\/voice\/)(.*?)('.*)/;
-    for (var i = 0; i < myListStrings.length; i++) 
-    {
-        routeId = myListStrings[i].match(regex);
-        audioSrc = myListStrings[i].match(regexAux);
-        var oldString = myListStrings[i];
-        if(myListStrings[i].match(regex))
-        {
-            myListStrings[i] = myListStrings[i].replace(myListStrings[i].match(regex)[1], `<a target="_blank" href="https://panel.binotel.com/?module=routes&companyID=${companyId}&action=edit&routeID=${routeId[1]}&type=callRules">${routeId[1]}</a>`);
-            myString = myString.replace(oldString,myListStrings[i]);
-        }
-        if(myListStrings[i].match(regexAux))
-        {
-            var audioSrc = myListStrings[i].match(regexAux);
-            var textBefore = audioSrc[1];
-            var textAfter = audioSrc[3];
-            var audioSrc = `https://panel.binotel.com/public/vOffice/${companyId}/origin/voice/${audioSrc[2].replace('.alaw','.wav').replace('.gsm','.wav')}`;
-
-            myListStrings[i] = oldString + `<audio controls src="${audioSrc}">${audioSrc[2].replace('.alaw','.wav')}</audio><br>`;
-            myString = myString.replace(oldString,myListStrings[i]);
-        }
-    }
+    
     listOfLogs.innerHTML = myString ;
+
+
 }
 var helpTooltips = document.getElementById('diagnosticTabWithFullLog').getElementsByClassName('helpTooltip');
 
